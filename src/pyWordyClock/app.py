e87e4f18ff17ae372de6bd118ff5163a94d1937b
kenlim <kennethlimfc@gmail.com>
2011-03-03 00:55:03 +0000

    Wired up to app

--- src/pyWordyClock/app.py
+++ src/pyWordyClock/app.py
@@ -1,5 +1,6 @@
 import sys
 from time import localtime
+from pyWordyClock.WordyClockConverter import convertToWords, roundToClosest5Minutes, roundToClosest5Minutes
 
 baseClock = """ITLISASTIME
 ACQUARTERDC
@@ -13,9 +14,9 @@ SEVENTWELVE
 TENSEOCLOCK"""
 
 def main():
-    print "Current time is: ", localtime()
-
-    print baseClock
+    clock = localtime()
+    hour, minutes = roundToClosest5Minutes(clock.tm_hour, clock.tm_min)
+    print convertToWords(hour, minutes)
     return 0
 
 if __name__ =='__main__':
